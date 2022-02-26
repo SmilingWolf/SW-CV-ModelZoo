9c9b2eaf3ff54ee016f4f1d237649b3676e029e4
SmilingWolf <lupo996@gmail.com>
2022-02-26 17:18:16 +0100

    Models/ConvNext: first commit
    Paper implementation starting from ResNet code. Not yet 100% sure it is correct to a tee.
    So far the "T" model has been holding up well in training with those settings.
    Committing before I start poking around and inadvertently break something.

---
+++ Models/ConvNext.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+
+from .layers import Base, CNNAttention
+
+
+def ResBlock(
+    x,
+    filters=64,
+    cnn_attention=None,
+    stochdepth_rate=0.0,
+    prefix=None,
+):
+    out = x
+
+    out = tf.keras.layers.DepthwiseConv2D(
+        kernel_size=7,
+        padding="same",
+        name=Base.format_name(prefix, "conv2d_01"),
+    )(out)
+    out = tf.keras.layers.LayerNormalization(name=Base.format_name(prefix, "norm_01"))(
+        out
+    )
+
+    out = tf.keras.layers.Conv2D(
+        filters=filters * 4,
+        kernel_size=1,
+        padding="same",
+        name=Base.format_name(prefix, "conv2d_02"),
+    )(out)
+    out = tf.keras.layers.Activation(
+        activation="gelu", name=Base.format_name(prefix, "act_02")
+    )(out)
+
+    out = tf.keras.layers.Conv2D(
+        filters=filters,
+        kernel_size=1,
+        padding="same",
+        name=Base.format_name(prefix, "conv2d_03"),
+    )(out)
+
+    if cnn_attention == "se":
+        out = CNNAttention.SEBlock(filters)(out)
+    elif cnn_attention == "eca":
+        out = CNNAttention.ECABlock(filters)(out)
+
+    if stochdepth_rate > 0.0:
+        out = Base.StochDepth(drop_rate=stochdepth_rate, scale_by_keep=True)(out)
+
+    out = Base.SkipInit()(out)
+
+    out = tf.keras.layers.Add()([out, x])
+    return out
+
+
+definitions = {
+    "T": {"blocks": [3, 3, 9, 3], "filters": [96, 192, 384, 768]},
+    "S": {"blocks": [3, 3, 27, 3], "filters": [96, 192, 384, 768]},
+    "B": {"blocks": [3, 3, 27, 3], "filters": [128, 256, 512, 1024]},
+    "L": {"blocks": [3, 3, 27, 3], "filters": [192, 384, 768, 1536]},
+}
+
+
+def ConvNextV1(
+    in_shape=(320, 320, 3),
+    out_classes=2000,
+    definition_name="S",
+    cnn_attention=None,
+    input_scaling="inception",
+):
+    stochdepth_rate = 0.1
+    definition = definitions[definition_name]
+
+    num_blocks = sum(definition["blocks"])
+
+    img_input = tf.keras.layers.Input(shape=in_shape)
+    x = Base.input_scaling(method=input_scaling)(img_input)
+
+    # Root block / "stem"
+    x = tf.keras.layers.Conv2D(
+        filters=definition["filters"][0],
+        kernel_size=4,
+        strides=4,
+        use_bias=True,
+        padding="same",
+        name="root_conv2d",
+    )(x)
+    x = tf.keras.layers.LayerNormalization(name="root_norm")(x)
+
+    index = 0
+    full_index = 0
+    for stage_depth, block_width in zip(definition["blocks"], definition["filters"]):
+        if index > 0:
+            x = tf.keras.layers.LayerNormalization(name="block%d_down_norm" % index)(
+                x
+            )
+            x = tf.keras.layers.Conv2D(
+                filters=block_width,
+                kernel_size=2,
+                strides=2,
+                use_bias=True,
+                padding="same",
+                name="block%d_down_conv2d" % index,
+            )(x)
+
+        for block_index in range(stage_depth):
+            block_stochdepth_rate = stochdepth_rate * full_index / num_blocks
+            x = ResBlock(
+                x,
+                block_width,
+                cnn_attention=cnn_attention,
+                stochdepth_rate=block_stochdepth_rate,
+                prefix="block%d_cell%d" % (index, block_index),
+            )
+            full_index += 1
+        index += 1
+
+    # Classification block
+    x = tf.keras.layers.GlobalAveragePooling2D(name="predictions_globalavgpooling")(x)
+    x = tf.keras.layers.LayerNormalization(name="predictions_norm")(x)
+
+    dense_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.01)
+    x = tf.keras.layers.Dense(
+        out_classes, kernel_initializer=dense_init, name="predictions_dense"
+    )(x)
+    x = tf.keras.layers.Activation("sigmoid", name="predictions_sigmoid")(x)
+
+    model = Model(img_input, x, name="ConvNext%sV1" % definition_name)
+    return model
---
+++ train_convnext.py
@@ -0,0 +1,150 @@
+from datetime import datetime
+
+import numpy as np
+import tensorflow as tf
+import wandb
+from tensorflow.keras.optimizers import SGD
+from tensorflow_addons.losses import SigmoidFocalCrossEntropy
+from tensorflow_addons.metrics import F1Score
+from wandb.keras import WandbCallback
+
+from Generator.ParseTFRecord import DataGenerator
+from Models.ConvNext import ConvNextV1
+
+
+def scheduler(epoch, lr):
+    if epoch < warmup_epochs:
+        return (
+            warmup_learning_rate
+            + ((max_learning_rate - warmup_learning_rate) / (warmup_epochs - 1)) * epoch
+        )
+    else:
+        cosine_decay = 0.5 * (
+            1
+            + tf.math.cos(
+                np.pi * (epoch - warmup_epochs) / (total_epochs - warmup_epochs)
+            )
+        )
+        alpha = final_learning_rate / max_learning_rate
+        decayed = (1 - alpha) * cosine_decay + alpha
+        return max_learning_rate * decayed
+
+
+if __name__ == "__main__":
+    try:  # detect TPUs
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+        strategy = tf.distribute.TPUStrategy(tpu)
+    except ValueError:  # detect GPUs
+        strategy = tf.distribute.MirroredStrategy()  # for CPU/GPU or multi-GPU machines
+
+    now = datetime.now()
+    date_time = now.strftime("%m_%d_%Y_%Hh%Mm%Ss")
+
+    node_name = "vm_name_here"
+    bucket_root = "gs://sw_tpu_training"
+
+    # Input
+    image_size = 320
+    total_labels = 2380
+    global_batch_size = 32 * strategy.num_replicas_in_sync
+
+    # Training schedule
+    warmup_epochs = 5
+    total_epochs = 100
+
+    # Learning rate
+    max_learning_rate = 0.02
+    warmup_learning_rate = max_learning_rate * 0.1
+    final_learning_rate = max_learning_rate * 0.01
+
+    # Model definition
+    definition_name = "T"
+    cnn_attention = None
+    activation = "gelu"
+
+    # Augmentations
+    noise_level = 2
+    mixup_alpha = 0.2
+    random_resize_method = True
+
+    train_config = {
+        "image_size": image_size,
+        "total_labels": total_labels,
+        "global_batch_size": global_batch_size,
+        "warmup_epochs": warmup_epochs,
+        "total_epochs": total_epochs,
+        "max_learning_rate": max_learning_rate,
+        "warmup_learning_rate": warmup_learning_rate,
+        "final_learning_rate": final_learning_rate,
+        "definition_name": definition_name,
+        "cnn_attention": cnn_attention,
+        "activation": activation,
+        "noise_level": noise_level,
+        "mixup_alpha": mixup_alpha,
+        "random_resize_method": random_resize_method,
+    }
+
+    wandb_run = wandb.init(
+        project="tpu-tracking",
+        entity="smilingwolf",
+        config=train_config,
+        name="ConvNext%sV1_%s" % (definition_name, date_time),
+        tags=[node_name],
+    )
+
+    training_generator = DataGenerator(
+        "%s/%s/record_shards_train/*" % (bucket_root, node_name),
+        total_labels=total_labels,
+        image_size=image_size,
+        batch_size=global_batch_size,
+        noise_level=noise_level,
+        mixup_alpha=mixup_alpha,
+        random_resize_method=random_resize_method,
+    )
+    training_dataset = training_generator.genDS()
+
+    validation_generator = DataGenerator(
+        "%s/%s/record_shards_val/*" % (bucket_root, node_name),
+        total_labels=total_labels,
+        image_size=image_size,
+        batch_size=global_batch_size,
+        noise_level=0,
+        mixup_alpha=0.0,
+        random_resize_method=False,
+    )
+    validation_dataset = validation_generator.genDS()
+
+    with strategy.scope():
+        model = ConvNextV1(
+            in_shape=(image_size, image_size, 3),
+            out_classes=total_labels,
+            definition_name=definition_name,
+            cnn_attention=cnn_attention,
+            input_scaling="inception",
+        )
+
+        f1 = F1Score(total_labels, "micro", 0.4)
+        loss = SigmoidFocalCrossEntropy(reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE)
+        opt = SGD(learning_rate=warmup_learning_rate, momentum=0.9, nesterov=True)
+        model.compile(optimizer=opt, loss=loss, metrics=[f1])
+
+    sched = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose=True)
+    rmc_loss = tf.keras.callbacks.ModelCheckpoint(
+        "%s/checkpoints/ConvNext%sV1_%s/variables/variables" % (bucket_root, definition_name, date_time),
+        save_best_only=True,
+        save_freq="epoch",
+        save_weights_only=True,
+    )
+    cb_list = [rmc_loss, sched, WandbCallback(save_model=False)]
+
+    history = model.fit(
+        training_dataset,
+        validation_data=validation_dataset,
+        initial_epoch=0,
+        epochs=total_epochs,
+        steps_per_epoch=10996,
+        validation_steps=364,
+        callbacks=cb_list,
+    )
+
+    wandb_run.finish()
