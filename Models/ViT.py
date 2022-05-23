5d79f313fb6a3f37c41012a13a1e675b7a585230
SmilingWolf <lupo996@gmail.com>
2022-05-23 18:44:15 +0200

    ViT: import model and training code
    Implementation uses a few tricks from newer papers
    and uses GAP instead of class token currently

---
+++ Models/ViT.py
@@ -0,0 +1,161 @@
+import tensorflow as tf
+from tensorflow.keras.models import Model
+
+from .layers import Base
+
+
+class StochDepth(tf.keras.Model):
+    """Batchwise Dropout used in EfficientNet, optionally sans rescaling."""
+
+    def __init__(self, drop_rate, scale_by_keep=False, **kwargs):
+        super().__init__(**kwargs)
+        self.drop_rate = drop_rate
+        self.scale_by_keep = scale_by_keep
+
+    def call(self, x, training):
+        if not training:
+            return x
+
+        batch_size = tf.shape(x)[0]
+        r = tf.random.uniform(shape=[batch_size, 1, 1], dtype=x.dtype)
+        keep_prob = 1.0 - self.drop_rate
+        binary_tensor = tf.floor(keep_prob + r)
+        if self.scale_by_keep:
+            x = x / keep_prob
+        return x * binary_tensor
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"drop_rate": self.drop_rate})
+        config.update({"scale_by_keep": self.scale_by_keep})
+        return config
+
+
+class PosEmbed(tf.keras.layers.Layer):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        emb_init = tf.keras.initializers.TruncatedNormal(mean=0.0, stddev=0.02)
+        self.pos_embed = self.add_weight(
+            name="pos_embed",
+            shape=(input_shape[-2], input_shape[-1]),
+            initializer=emb_init,
+            trainable=True,
+        )
+
+    def call(self, x):
+        return x + self.pos_embed
+
+
+def MLPBlock(x, mlp_dim, prefix=""):
+    out = tf.keras.layers.Dense(mlp_dim, name=f"{prefix}_dense_01")(x)
+    out = tf.keras.layers.Activation(activation="gelu", name=f"{prefix}_act_01")(out)
+    out = tf.keras.layers.Dense(x.shape[-1], name=f"{prefix}_dense_02")(out)
+    return out
+
+
+def ViTBlock(x, heads, key_dim, mlp_dim, layerscale_init, stochdepth_rate, prefix=""):
+    out = x
+
+    out = tf.keras.layers.LayerNormalization(name=f"{prefix}_norm_01")(out)
+    out = tf.keras.layers.MultiHeadAttention(num_heads=heads, key_dim=key_dim // heads)(
+        out, out
+    )
+    out = Base.SkipInitChannelwise(channels=key_dim, init_val=layerscale_init)(out)
+
+    if stochdepth_rate > 0.0:
+        out = StochDepth(stochdepth_rate, scale_by_keep=True)(out)
+
+    x = tf.keras.layers.Add()([out, x])
+
+    out = tf.keras.layers.LayerNormalization(name=f"{prefix}_norm_02")(x)
+    out = MLPBlock(out, mlp_dim, prefix=f"{prefix}_cm")
+    out = Base.SkipInitChannelwise(channels=key_dim, init_val=layerscale_init)(out)
+
+    if stochdepth_rate > 0.0:
+        out = StochDepth(stochdepth_rate, scale_by_keep=True)(out)
+
+    out = tf.keras.layers.Add()([out, x])
+    return out
+
+
+def CaiT_LayerScale_init(network_depth):
+    if network_depth <= 18:
+        return 1e-1
+    elif network_depth <= 24:
+        return 1e-5
+    else:
+        return 1e-6
+
+
+definitions = {
+    "B16": {
+        "num_blocks": 12,
+        "patch_size": 16,
+        "key_dim": 768,
+        "mlp_dim": 3072,
+        "heads": 12,
+        "stochdepth_rate": 0.05,
+    },
+    "B32": {
+        "num_blocks": 12,
+        "patch_size": 32,
+        "key_dim": 768,
+        "mlp_dim": 3072,
+        "heads": 12,
+        "stochdepth_rate": 0.05,
+    },
+    "L16": {
+        "num_blocks": 24,
+        "patch_size": 16,
+        "key_dim": 1024,
+        "mlp_dim": 4096,
+        "heads": 16,
+        "stochdepth_rate": 0.2,
+    },
+}
+
+
+def ViT(
+    in_shape=(320, 320, 3),
+    out_classes=2000,
+    definition_name="B16",
+    input_scaling="inception",
+):
+    definition = definitions[definition_name]
+    num_blocks = definition["num_blocks"]
+    patch_size = definition["patch_size"]
+    key_dim = definition["key_dim"]
+    mlp_dim = definition["mlp_dim"]
+    heads = definition["heads"]
+    stochdepth_rate = definition["stochdepth_rate"]
+    layerscale_init = CaiT_LayerScale_init(num_blocks)
+
+    img_input = tf.keras.layers.Input(shape=in_shape)
+    x = Base.input_scaling(method=input_scaling)(img_input)
+
+    prefix = "root"
+    x = tf.keras.layers.Conv2D(
+        filters=key_dim,
+        kernel_size=patch_size,
+        strides=patch_size,
+        padding="same",
+        name=f"{prefix}_conv2d_01",
+    )(x)
+    x = tf.keras.layers.Reshape(target_shape=(-1, key_dim))(x)
+    x = PosEmbed()(x)
+
+    for i in range(num_blocks):
+        prefix = f"block{i}"
+        x = ViTBlock(
+            x, heads, key_dim, mlp_dim, layerscale_init, stochdepth_rate, prefix
+        )
+
+    x = tf.keras.layers.LayerNormalization(name="predictions_norm")(x)
+    x = tf.keras.layers.GlobalAveragePooling1D(name="predictions_globalavgpooling")(x)
+    x = tf.keras.layers.Dense(out_classes, name="predictions_dense")(x)
+    x = tf.keras.layers.Activation("sigmoid", name="predictions_sigmoid")(x)
+
+    model = Model(img_input, x, name=f"ViT-{definition_name}")
+    return model
---
+++ train_vit.py
@@ -0,0 +1,178 @@
+from datetime import datetime
+
+import numpy as np
+import tensorflow as tf
+import wandb
+from tensorflow_addons.metrics import F1Score
+from tensorflow_addons.optimizers import LAMB
+from wandb.keras import WandbCallback
+
+from Generator.ParseTFRecord import DataGenerator
+from Losses.ASL import AsymmetricLoss
+from Models.ViT import ViT
+
+
+def scheduler(epoch, lr):
+    if epoch < warmup_epochs:
+        return (
+            warmup_learning_rate
+            + ((max_learning_rate - warmup_learning_rate) / (warmup_epochs - 1)) * epoch
+        )
+    else:
+        cosine_decay = 0.5 * (
+            1
+            + tf.math.cos(
+                np.pi * (epoch - warmup_epochs) / (total_epochs - warmup_epochs)
+            )
+        )
+        alpha = final_learning_rate / max_learning_rate
+        decayed = (1 - alpha) * cosine_decay + alpha
+        return max_learning_rate * decayed
+
+
+if __name__ == "__main__":
+    try:  # detect TPUs
+        tpu = tf.distribute.cluster_resolver.TPUClusterResolver.connect()
+        strategy = tf.distribute.TPUStrategy(tpu)
+    except ValueError:  # detect GPUs
+        strategy = tf.distribute.MirroredStrategy()  # for CPU/GPU or multi-GPU machines
+
+    now = datetime.now()
+    date_time = now.strftime("%m_%d_%Y_%Hh%Mm%Ss")
+
+    multiplier = 1
+    node_name = "vm_name_here"
+    bucket_root = "gs://sw_tpu_training"
+
+    # Input
+    image_size = 320
+    total_labels = 2380
+    global_batch_size = 32 * multiplier * strategy.num_replicas_in_sync
+
+    # Training schedule
+    warmup_epochs = 5
+    total_epochs = 100
+
+    # Learning rate
+    max_learning_rate = 5e-4 * multiplier
+    warmup_learning_rate = max_learning_rate * 0.1
+    final_learning_rate = max_learning_rate * 0.01
+    weight_decay_rate = 0.05
+
+    # Model definition
+    definition_name = "B16"
+    cnn_attention = None
+    activation = "gelu"
+
+    # Augmentations
+    noise_level = 2
+    mixup_alpha = 0.8
+    cutout_max_pct = 0.0
+    random_resize_method = True
+
+    # Loss
+    loss_name = "asl"
+    asl_gamma_neg = 0
+    asl_gamma_pos = 0
+    asl_clip = 0.0
+
+    train_config = {
+        "image_size": image_size,
+        "total_labels": total_labels,
+        "global_batch_size": global_batch_size,
+        "warmup_epochs": warmup_epochs,
+        "total_epochs": total_epochs,
+        "max_learning_rate": max_learning_rate,
+        "warmup_learning_rate": warmup_learning_rate,
+        "final_learning_rate": final_learning_rate,
+        "weight_decay_rate": weight_decay_rate,
+        "definition_name": definition_name,
+        "cnn_attention": cnn_attention,
+        "activation": activation,
+        "noise_level": noise_level,
+        "mixup_alpha": mixup_alpha,
+        "cutout_max_pct": cutout_max_pct,
+        "random_resize_method": random_resize_method,
+        "loss_name": loss_name,
+        "asl_gamma_neg": asl_gamma_neg,
+        "asl_gamma_pos": asl_gamma_pos,
+        "asl_clip": asl_clip,
+    }
+
+    wandb_run = wandb.init(
+        project="tpu-tracking",
+        entity="smilingwolf",
+        config=train_config,
+        name="ViT%s_%s" % (definition_name, date_time),
+        tags=[node_name],
+    )
+
+    training_generator = DataGenerator(
+        "%s/%s/record_shards_train/*" % (bucket_root, node_name),
+        total_labels=total_labels,
+        image_size=image_size,
+        batch_size=global_batch_size,
+        noise_level=noise_level,
+        mixup_alpha=mixup_alpha,
+        cutout_max_pct=cutout_max_pct,
+        random_resize_method=random_resize_method,
+    )
+    training_dataset = training_generator.genDS()
+
+    validation_generator = DataGenerator(
+        "%s/%s/record_shards_val/*" % (bucket_root, node_name),
+        total_labels=total_labels,
+        image_size=image_size,
+        batch_size=global_batch_size,
+        noise_level=0,
+        mixup_alpha=0.0,
+        cutout_max_pct=0.0,
+        random_resize_method=False,
+    )
+    validation_dataset = validation_generator.genDS()
+
+    with strategy.scope():
+        model = ViT(
+            in_shape=(image_size, image_size, 3),
+            out_classes=total_labels,
+            definition_name=definition_name,
+            input_scaling="inception",
+        )
+
+        f1 = F1Score(total_labels, "micro", 0.4)
+        rec_at_p65 = tf.keras.metrics.RecallAtPrecision(0.65, num_thresholds=1024)
+        loss = AsymmetricLoss(
+            reduction=tf.keras.losses.Reduction.SUM_OVER_BATCH_SIZE,
+            gamma_neg=asl_gamma_neg,
+            gamma_pos=asl_gamma_pos,
+            clip=asl_clip,
+        )
+        opt = LAMB(
+            learning_rate=warmup_learning_rate,
+            weight_decay_rate=weight_decay_rate,
+            exclude_from_weight_decay=["norm", "skip", "pos_embed"],
+        )
+        model.compile(optimizer=opt, loss=loss, metrics=[f1, rec_at_p65])
+
+    t800 = tf.keras.callbacks.TerminateOnNaN()
+    sched = tf.keras.callbacks.LearningRateScheduler(scheduler, verbose=True)
+    rmc_loss = tf.keras.callbacks.ModelCheckpoint(
+        "%s/checkpoints/ViT%s_%s/variables/variables"
+        % (bucket_root, definition_name, date_time),
+        save_best_only=True,
+        save_freq="epoch",
+        save_weights_only=True,
+    )
+    cb_list = [t800, rmc_loss, sched, WandbCallback(save_model=False)]
+
+    history = model.fit(
+        training_dataset,
+        validation_data=validation_dataset,
+        initial_epoch=0,
+        epochs=total_epochs,
+        steps_per_epoch=10996 // multiplier,
+        validation_steps=364 // multiplier,
+        callbacks=cb_list,
+    )
+
+    wandb_run.finish()
