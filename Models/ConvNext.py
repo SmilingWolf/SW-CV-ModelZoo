71dead41f913c17cd4629349053a58a1387651f7
SmilingWolf <lupo996@gmail.com>
2023-03-12 09:25:12 +0100

    Models/ConvNext: support ConvNext V2
    Add GRN layer, add model def.
    Also cleanup some string silliness.

--- Models/ConvNext.py
+++ Models/ConvNext.py
@@ -27,13 +27,40 @@ class Affine(tf.keras.layers.Layer):
         return self.alpha * x + self.beta
 
 
+class GlobalResponseNorm(tf.keras.layers.Layer):
+    def __init__(self, eps=1e-6, **kwargs):
+        super().__init__(**kwargs)
+        self.eps = eps
+
+    def build(self, input_shape):
+        channels = input_shape[-1]
+        self.beta = self.add_weight(
+            name="beta",
+            shape=(1, 1, 1, channels),
+            initializer="zeros",
+            trainable=True,
+        )
+        self.gamma = self.add_weight(
+            name="gamma",
+            shape=(1, 1, 1, channels),
+            initializer="zeros",
+            trainable=True,
+        )
+
+    def call(self, x):
+        gx = tf.norm(x, ord="euclidean", axis=(1, 2), keepdims=True)
+        nx = gx / (tf.math.reduce_mean(gx, axis=-1, keepdims=True) + self.eps)
+        return self.gamma * (x * nx) + self.beta + x
+
+
 def ResBlock(
     x,
+    prefix,
     filters=64,
     cnn_attention=None,
     stochdepth_rate=0.0,
     norm_func=tf.keras.layers.LayerNormalization,
-    prefix=None,
+    version=1,
 ):
     out = x
 
@@ -41,25 +68,20 @@ def ResBlock(
     out = tf.keras.layers.DepthwiseConv2D(
         kernel_size=7,
         padding="valid",
-        name=Base.format_name(prefix, "conv2d_01"),
+        name=f"{prefix}_conv2d_01",
     )(out)
-    out = norm_func(name=Base.format_name(prefix, "norm_01"))(out)
+    out = norm_func(name=f"{prefix}_norm_01")(out)
 
     out = tf.keras.layers.Conv2D(
-        filters=filters * 4,
-        kernel_size=1,
-        padding="same",
-        name=Base.format_name(prefix, "conv2d_02"),
+        filters=filters * 4, kernel_size=1, padding="same", name=f"{prefix}_conv2d_02"
     )(out)
-    out = tf.keras.layers.Activation(
-        activation="gelu", name=Base.format_name(prefix, "act_02")
-    )(out)
+    out = tf.keras.layers.Activation(activation="gelu", name=f"{prefix}_act_02")(out)
+
+    if version == 2:
+        out = GlobalResponseNorm(name=f"{prefix}_grn")(out)
 
     out = tf.keras.layers.Conv2D(
-        filters=filters,
-        kernel_size=1,
-        padding="same",
-        name=Base.format_name(prefix, "conv2d_03"),
+        filters=filters, kernel_size=1, padding="same", name=f"{prefix}_conv2d_03"
     )(out)
 
     if cnn_attention == "se":
@@ -70,7 +92,8 @@ def ResBlock(
     if stochdepth_rate > 0.0:
         out = Base.StochDepth(drop_rate=stochdepth_rate, scale_by_keep=True)(out)
 
-    out = Base.SkipInitChannelwise(filters)(out)
+    if version == 1:
+        out = Base.SkipInitChannelwise(filters)(out)
 
     out = tf.keras.layers.Add()([out, x])
     return out
@@ -90,14 +113,15 @@ model_norms = {
 }
 
 
-def ConvNextV1(
-    in_shape=(320, 320, 3),
-    out_classes=2000,
-    definition_name="S",
-    cnn_attention=None,
-    input_scaling="inception",
-    stochdepth_rate=0.1,
-    norm="layernorm",
+def ConvNext(
+    in_shape,
+    out_classes,
+    definition_name,
+    cnn_attention,
+    input_scaling,
+    stochdepth_rate,
+    norm,
+    version,
 ):
     definition = definitions[definition_name]
     norm_func = model_norms[norm]
@@ -129,18 +153,19 @@ def ConvNextV1(
                 strides=2,
                 use_bias=True,
                 padding="same",
-                name="block%d_down_conv2d" % index,
+                name=f"block{index}_down_conv2d",
             )(x)
 
         for block_index in range(stage_depth):
             block_stochdepth_rate = stochdepth_rate * full_index / num_blocks
             x = ResBlock(
                 x,
-                block_width,
+                prefix=f"block{index}_cell{block_index}",
+                filters=block_width,
                 cnn_attention=cnn_attention,
                 stochdepth_rate=block_stochdepth_rate,
                 norm_func=norm_func,
-                prefix="block%d_cell%d" % (index, block_index),
+                version=version,
             )
             full_index += 1
         index += 1
@@ -155,5 +180,47 @@ def ConvNextV1(
     )(x)
     x = tf.keras.layers.Activation("sigmoid", name="predictions_sigmoid")(x)
 
-    model = Model(img_input, x, name="ConvNext%sV1" % definition_name)
+    model = Model(img_input, x, name=f"ConvNext{definition_name}V{version}")
     return model
+
+
+def ConvNextV1(
+    in_shape=(320, 320, 3),
+    out_classes=2000,
+    definition_name="S",
+    cnn_attention=None,
+    input_scaling="inception",
+    stochdepth_rate=0.1,
+    norm="layernorm",
+):
+    return ConvNext(
+        in_shape=in_shape,
+        out_classes=out_classes,
+        definition_name=definition_name,
+        cnn_attention=cnn_attention,
+        input_scaling=input_scaling,
+        stochdepth_rate=stochdepth_rate,
+        norm=norm,
+        version=1,
+    )
+
+
+def ConvNextV2(
+    in_shape=(320, 320, 3),
+    out_classes=2000,
+    definition_name="S",
+    cnn_attention=None,
+    input_scaling="inception",
+    stochdepth_rate=0.1,
+    norm="layernorm",
+):
+    return ConvNext(
+        in_shape=in_shape,
+        out_classes=out_classes,
+        definition_name=definition_name,
+        cnn_attention=cnn_attention,
+        input_scaling=input_scaling,
+        stochdepth_rate=stochdepth_rate,
+        norm=norm,
+        version=2,
+    )
