e3b67ac58f0d03fd97020d984d1297e887185957
SmilingWolf <lupo996@gmail.com>
2023-05-21 22:41:05 +0200

    train_moat: slightly better LR

--- train_moat.py
+++ train_moat.py
@@ -48,7 +48,7 @@ if __name__ == "__main__":
     total_epochs = 100
 
     # Learning rate
-    max_learning_rate = 2e-4 * multiplier
+    max_learning_rate = 4e-4 * multiplier
     warmup_learning_rate = max_learning_rate * 0.1
     final_learning_rate = max_learning_rate * 0.01
     weight_decay_rate = 0.05
